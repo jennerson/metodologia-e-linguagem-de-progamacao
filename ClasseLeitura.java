fb90b9d4b05fd5fa71793802071fffeb59be0737
jennerson <47647956+jennerson@users.noreply.github.com>
2019-08-20 21:36:21 -0300

    aula1

---
+++ ClasseLeitura.java
@@ -0,0 +1,23 @@
+package com.jennerson.unipe.java.aula1;
+
+import java.util.Scanner;
+
+public class ClasseLeitura {
+	
+	public static void main(String[] args) {
+		
+		int idade = 0;
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Informe sua idade: ");
+		idade = leitor.nextInt();
+		
+		if(idade>=18){
+			System.out.println("Pode passar!!!");
+		}else{
+			System.out.println("Vai tirar a catinga do mijo!!!");
+		}
+	}
+	
+}
